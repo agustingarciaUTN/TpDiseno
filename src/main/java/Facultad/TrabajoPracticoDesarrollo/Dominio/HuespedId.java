b620b50d0051864d1e240326c733df8e1a62bbfd
Matias Javier Decolatti <mdecolatti@frsf.utn.edu.ar>
2025-12-07 12:19:39 -0300

    Agregado HuespedId.java

---
+++ src/main/java/Facultad/TrabajoPracticoDesarrollo/Dominio/HuespedId.java
@@ -0,0 +1,35 @@
+package Facultad.TrabajoPracticoDesarrollo.Dominio;
+
+import Facultad.TrabajoPracticoDesarrollo.enums.TipoDocumento;
+import java.io.Serializable;
+import java.util.Objects;
+
+//Clase de Clave Compuesta
+
+//Necesaria porque la tabla Huesped usa tipo_documento + numero_documento como PK.
+
+
+public class HuespedId implements Serializable {
+    private TipoDocumento tipoDocumento;
+    private String nroDocumento;
+
+    public HuespedId() {}
+
+    public HuespedId(TipoDocumento tipoDocumento, String nroDocumento) {
+        this.tipoDocumento = tipoDocumento;
+        this.nroDocumento = nroDocumento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HuespedId that = (HuespedId) o;
+        return tipoDocumento == that.tipoDocumento && Objects.equals(nroDocumento, that.nroDocumento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tipoDocumento, nroDocumento);
+    }
+}
