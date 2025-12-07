581aa5cf3bff0ba6374ffbd2877064fedd8f0dcf
Marcos <mgianni@frsf.utn.edu.ar>
2025-12-07 19:13:49 -0300

    Buscar agregado a HuespedController

--- src/main/java/Facultad/TrabajoPracticoDesarrollo/Controllers/HuespedController.java
+++ src/main/java/Facultad/TrabajoPracticoDesarrollo/Controllers/HuespedController.java
@@ -1,10 +1,14 @@
 package Facultad.TrabajoPracticoDesarrollo.Controllers;
 
+import Facultad.TrabajoPracticoDesarrollo.Dominio.Huesped;
 import Facultad.TrabajoPracticoDesarrollo.Huesped.DtoHuesped;
 import Facultad.TrabajoPracticoDesarrollo.Huesped.GestorHuesped;
 import jakarta.validation.Valid; // Importante
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/huespedes")
@@ -15,6 +19,23 @@ public class HuespedController {
     // 2. Inyección por Constructor (Spring te pasa el Gestor listo)
     public HuespedController(GestorHuesped gestorHuesped) {
         this.gestorHuesped = gestorHuesped;
+    }
+
+    @PostMapping("/buscar")
+    public ResponseEntity<List<Huesped>> buscarHuesped(@Valid @RequestBody(required = false) DtoHuesped criterios) {
+        try {
+            // Manejo de criterios nulos (para traer todos si no se envía body)
+            if (criterios == null) {
+                criterios = new DtoHuesped();
+            }
+            ArrayList<Huesped> listaHuespedes = gestorHuesped.buscarHuespedes(criterios);
+
+            return ResponseEntity.ok(listaHuespedes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+
     }
 
     @PostMapping("/crear")
@@ -39,4 +60,5 @@ public class HuespedController {
     public String test() {
         return "Controller de Huéspedes activo";
     }
+
 }
