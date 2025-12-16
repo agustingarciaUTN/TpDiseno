e5c866c35b2794608e2b515855e0facf55b15ca2
AgustinGarciaUTN <aggarcia@frsf.utn.edu.ar>
2025-12-16 08:18:08 -0700

    borre lo inutil, dao, gestores y pantalla

--- src/main/java/Facultad/TrabajoPracticoDesarrollo/Repositories/DAOs/DaoInterfazNotaDeCredito.java
+++
@@ -1,9 +0,0 @@
-//package Facultad.TrabajoPracticoDesarrollo.Repositories.DAOs;
-//import Facultad.TrabajoPracticoDesarrollo.Dominio.NotaDeCredito;
-//import Facultad.TrabajoPracticoDesarrollo.Excepciones.PersistenciaException;
-//import Facultad.TrabajoPracticoDesarrollo.DTOs.DtoNotaDeCredito;
-//
-//public interface DaoInterfazNotaDeCredito {
-//    boolean persistirNota(NotaDeCredito nota) throws PersistenciaException;
-//    DtoNotaDeCredito obtenerPorNumero(String numero);
-//}
